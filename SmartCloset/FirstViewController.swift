bd1fba7db34aa9e324bc6bec6e3418a355c0e6f8
Elvis Lam <elvislam@ElvistekiMacBook-Air.local>
2015-01-08 16:46:21 +0800

    Initial Commit

---
+++ SmartCloset/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SmartCloset
+//
+//  Created by Elvis Lam on 8/1/15.
+//  Copyright (c) 2015 Elvis Lam. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SmartClosetTests/SmartClosetTests.swift
@@ -0,0 +1,36 @@
+//
+//  SmartClosetTests.swift
+//  SmartClosetTests
+//
+//  Created by Elvis Lam on 8/1/15.
+//  Copyright (c) 2015 Elvis Lam. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SmartClosetTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
